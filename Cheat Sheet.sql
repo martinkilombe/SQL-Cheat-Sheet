6714479c2dfbbee62bb5caf0741f0f22facc0ade
Martin Muti Kilombe <martinkilombe@gmail.com>
2023-07-12 13:22:23 +0300

    Used AND

--- Cheat Sheet.sql
+++ Cheat Sheet.sql
@@ -1,1 +1,15 @@
 SELECT * FROM electric;
+
+--Filtering Output -
+--Comparison Operators
+SELECT "Model" FROM electric
+WHERE "Electric_Range"
+
+--Like
+SELECT "Make", "Model" FROM electric
+WHERE "Make" LIKE '%TESLA' 
+
+--Counting the Number of Vehicles(COUNT) between 2015 AND 2021
+SELECT COUNT("Model_Year") FROM electric
+WHERE "Model_Year" BETWEEN 2015 AND 2021
+
