957d27e8578322bbefc77f98ea28380e4ccd12fc
Martin Muti Kilombe <martinkilombe@gmail.com>
2023-07-12 14:42:16 +0300

    SUB-QUERIES

--- Cheat Sheet.sql
+++ Cheat Sheet.sql
@@ -9,7 +9,24 @@ WHERE "Electric_Range"
 SELECT "Make", "Model" FROM electric
 WHERE "Make" LIKE '%TESLA' 
 
---Counting the Number of Vehicles(COUNT) between 2015 AND 2021
+
+
+--AGGREGATE FUNCTIONS
+--(COUNT)
 SELECT COUNT("Model_Year") FROM electric
 WHERE "Model_Year" BETWEEN 2015 AND 2021
+--DISTINCT(COUNT)
+SELECT "Make", COUNT(DISTINCT "Make")FROM electric
+GROUP BY "Make"
 
+--MIN(),MAX()
+SELECT MIN("Electric_Range")as Minimum_Mileage, Max("Electric_Range")as Maximum_Mileage FROM electric;
+
+--AVG(), GROUP BY(), HAVING()
+SELECT "Vehicle_ID", AVG("Electric_Range") FROM electric
+GROUP BY "Vehicle_ID"
+HAVING AVG("Electric_Range")>50;
+
+
+--SUBQUERIES
+SELECT ss
